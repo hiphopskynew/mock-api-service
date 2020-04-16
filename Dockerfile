df3e6ecdf77933ff4f217e6ccabc28d8bf884d45
Wuttikrai Limsakul <wuttikrai.l@kbtg.tech>
2020-04-16 18:07:18 +0700

    update route & deployment

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN cd /src && go build -o mock-api-service
 
 FROM alpine
 WORKDIR /services
-COPY --from=build-env /src/mock-api-service /services/.
+COPY --from=build-env /src/mock-api-service /services/mock-api-service
+COPY --from=build-env /src/config/config.yml /services/config/config.yml
 CMD ./mock-api-service
--- route/route.go
+++ route/route.go
@@ -3,6 +3,7 @@ package route
 import (
 	"net/http"
 	"sort"
+	"strings"
 
 	"github.com/hiphopskynew/mock-api-service/config"
 	"github.com/hiphopskynew/mock-api-service/service"
@@ -30,19 +31,32 @@ func RegisterRoutes(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
 		rs := routes(e.Routes())
 		sort.Sort(rs)
-		return c.JSON(http.StatusOK, rs)
+		filtered := routes{}
+		for _, r := range rs {
+			if strings.HasPrefix(r.Name, "github.com/labstack/echo.(*Group)") || r.Path == "/*" {
+				continue
+			}
+			filtered = append(filtered, r)
+		}
+		return c.JSON(http.StatusOK, filtered)
 	}).Name = "inquiry available apis"
 
-	for _, r := range e.Any("/*", service.InvokeConfig) {
+	ers := e.Any("/*", service.InvokeConfig)
+	for _, r := range ers {
 		r.Name = "all supported path see in /admin/api/manage/settings"
 	}
+
+	registerAdminManagement(e)
+}
+
+func registerAdminManagement(e *echo.Echo) {
+	e.GET("/admin/api/manage/settings/:id", service.GetSetting).Name = "inquiry a setting"
+	e.GET("/admin/api/manage/settings", service.GetSettingList).Name = "inquiry setting list"
 
 	ge := e.Group("/admin/api/manage")
 	ge.Use(middleware.JWT([]byte(config.Config.JWT.Secret)))
 
 	ge.POST("/settings", service.AddSetting).Name = `create a new setting, example request body {'uri':'/api/v1/mock/inquiry','method':'GET','response':{'header':{'hmock':'Wuttikrai Limsakul','content-type':'application/json'},'body':{'a':'','b':0,'c':[],'d':{}},'code':200}}`
-	ge.GET("/settings/:id", service.GetSetting).Name = "inquiry a setting"
-	ge.GET("/settings", service.GetSettingList).Name = "inquiry setting list"
 	ge.PUT("/settings/:id", service.UpdateSetting).Name = `update a setting, example request body {'uri':'/api/v1/mock/inquiry','method':'GET','response':{'header':{'hmock':'Wuttikrai Limsakul','content-type':'application/json'},'body':{'a':'','b':0,'c':[],'d':{}},'code':200}}`
 	ge.DELETE("/settings/:id", service.DeleteSetting).Name = "delete a setting"
 }
